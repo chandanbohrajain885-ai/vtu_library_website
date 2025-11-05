0ed51f0b39750901e5bb7be989d8828e149a3df6
Wix Remote Machine <support@wix.com>
2025-11-05 14:53:52 +0000

    Replace 2023-24 E-Resources with styled user data

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -23,20 +23,6 @@ export interface EResources {
   title?: string;
   /** @wixFieldType text */
   1?: string;
-  /** @wixFieldType text */
-  category?: string;
-  /** @wixFieldType text */
-  resourceTitle?: string;
-  /** @wixFieldType text */
-  author?: string;
-  /** @wixFieldType text */
-  description?: string;
-  /** @wixFieldType datetime */
-  publicationDate?: Date | string;
-  /** @wixFieldType url */
-  resourceLink?: string;
-  /** @wixFieldType url */
-  coverImage?: string;
 }
 
 
